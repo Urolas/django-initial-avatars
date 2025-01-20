2e00ade2fd6a2128fd445fa9a6dc1d6dc64ef36c
sngov <sophanna.ngov@outlook.com>
2025-01-20 16:08:17 +0100

    Replaced url with path function

--- initial_avatars/urls.py
+++ initial_avatars/urls.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.conf.urls import url
+from django.urls import path
 from .views import avatar
 
 urlpatterns = [
-    url(r'^(?P<id>[0-9]+)/$', avatar, name='avatar'),
-    url(r'^(?P<id>[0-9]+)/(?P<size>[0-9]{2,3})/$', avatar, name='avatar'),
+    path('<int:id>/', avatar, name='avatar'),
+    path('<int:id>/<int:size>/', avatar, name='avatar'),
 ]
